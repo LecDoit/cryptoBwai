6f72b4ee12c3d9a53b97320cd3070434d5a47722
pikimaru <pikajunglist@gmail.com>
2025-02-21 17:13:33 +0100

    course finished

---
+++ app/(dashboard)/dashboard/stocks/actions.js
@@ -0,0 +1,50 @@
+'use server'
+import { cookies } from "next/headers"
+
+
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+export async function addStock(formData){
+    const stock = Object.fromEntries(formData)
+
+    const supabase = createServerActionClient({cookies})
+
+    const {data:{session}} = await supabase.auth.getSession()
+
+    const {error} = await supabase.from('trades')
+    .insert({
+        ...stock,
+        user_email:session.user.email
+    })
+
+    if (error){
+        throw new Error('Could not add the new trade')
+    }
+
+    revalidatePath('/stocks')
+    redirect('/dashboard')
+}
+
+
+export async function deleteTicket(id){
+
+
+    const supabase = createServerActionClient({cookies})
+
+
+    const {error} = await supabase.from('trades')
+    .delete()
+    .eq('id',id)
+
+
+    if (error){
+        throw new Error('Could not delete the trade')
+    }
+
+    revalidatePath('/stocks')
+    redirect('/dashboard')
+
+
+}
--- app/(dashboard)/dashboard/stocks/create/CreateForm.jsx
+++ app/(dashboard)/dashboard/stocks/create/CreateForm.jsx
@@ -1,77 +1,37 @@
-"use client"
-
-import { useRouter } from "next/navigation"
-import { useState } from "react"
+import SubmitButton from "@/app/components/SubmitButton"
+import { addStock } from "../actions"
 
 export default function CreateForm() {
-  const router = useRouter()
 
-  const [currency, setCurrency] = useState('')
-  const [amount, setAmount] = useState('')
-  const [buy, setBuy] = useState('')
-  const [status, setStatus] = useState('')
-
-  const [isLoading, setIsLoading] = useState(false)
-
-  const handleSubmit = async (e)  => {
-    e.preventDefault()
-    setIsLoading(true)
-
-    const newTrade = { currency, amount, buy}
-
-    const res = await fetch('http://localhost:3000/api/trades/', {
-      method: "POST",
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(newTrade)
-    })
-
-    const json = await res.json()
-
-    if (json.error){
-        console.log(json.error.message)
-        setIsLoading(false)
-    }
-    if (json.data){
-        router.refresh()
-        router.push('/dashboard')
-        
-    }
-
-  }
+  
   return (
-    <form onSubmit={handleSubmit} className="w-1/2">
+    <form action={addStock} className="w-1/2">
       <label>
         <span>Currency:</span>
         <input
+          name="currency"
           required 
           type="text"
-          onChange={(e) => setCurrency(e.target.value)}
-          value={currency}
+
         />
       </label>
       <label>
         <span>Amount:</span>
         <textarea
+          name="amount"
           required
-          onChange={(e) => setAmount(e.target.value)}
-          value={amount}
+
         />
       </label>
       <label>
         <span>buy:</span>
         <textarea
+          name="buy"
           required
-          onChange={(e) => setBuy(e.target.value)}
-          value={buy}
+
         />
       </label>
-      <button 
-        className="btn-primary" 
-        disabled={isLoading}
-      >
-      {isLoading && <span>Adding...</span>}
-      {!isLoading && <span>Add Ticket</span>}
-    </button>
+      <SubmitButton/>
     </form>
   )
 }
