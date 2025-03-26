f1e78e522173680e5d078a4776d0a244c2fc4fa7
pikimaru <pikajunglist@gmail.com>
2025-03-26 11:28:19 +0100

    login works

--- app/(getin)/getin/layout.jsx
+++ app/(getin)/getin/layout.jsx
@@ -1,37 +1,8 @@
 
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import {cookies} from 'next/headers'
-import {redirect} from 'next/navigation'
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
-import { BellRing, Check } from "lucide-react"
- 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-
- 
-const notifications = [
-  {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
-  },
-  {
-    title: "You have a new message!",
-    description: "1 hour ago",
-  },
-  {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
-  },
-]
 
 
 
@@ -51,22 +22,7 @@ export default async function DashboardLayout({children}) {
       
         {children}
 
-    <Card className={cn("w-[380px]")} >
-      <CardHeader>
-        <CardTitle>Log In</CardTitle>
-        <CardDescription>Click here to Log In.</CardDescription>
-      </CardHeader>
-
-
-      <CardFooter>
-        <Button className="w-full">
-          <Check /> Log In
-        </Button>
-        <Button className="w-full">
-          <Check /> Sign Up
-        </Button>
-      </CardFooter>
-    </Card>
+   
 
     </div>
 
--- app/(getin)/getin/page.jsx
+++ app/(getin)/getin/page.jsx
@@ -3,6 +3,23 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import {cookies} from 'next/headers'
 import {redirect} from 'next/navigation'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+import { BellRing, Check } from "lucide-react"
+ 
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import Link from 'next/link'
+
+ 
+
 
 
 export default async function GetIn() {
@@ -19,8 +36,27 @@ export default async function GetIn() {
 //   }
 
   return (
-    <main>
-getin page
+    <main className="flex justify-center items-center min-h-screen">
+
+    <Card className={"w-[380px]"} >
+      <CardHeader>
+        <CardTitle>Get In!</CardTitle>
+        <CardDescription>Click here to Log in or Create New Account.</CardDescription>
+      </CardHeader>
+
+      <CardFooter className={'flex justify-center gap-4'}>
+        <Link className='w-full'  href={'/login'}>
+          <Button className='w-full' >
+           Log In
+          </Button>
+        </Link>
+        <Link  className='w-full'  href={'/signup'} >
+          <Button className='w-full' >
+            Sign Up
+          </Button>
+        </Link>
+      </CardFooter>
+    </Card>
 
     </main>
   )
